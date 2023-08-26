64a9a9eb5d5f66bd2240f09f91f38cbe4dea4d8f
Jrich45 <127894540+Jrich45@users.noreply.github.com>
2023-08-26 14:19:12 -0400

    Made the mason function

--- script.js
+++ script.js
@@ -23,11 +23,23 @@ function multiply(num, num2) {
 
 smell() */
 
-const invite = document.querySelector("#max-invite");
+/* const invite = document.querySelector("#max-invite");
 const output = document.querySelector(".invite-output");
 
 output.textContent = invite.value;
 
 invite.addEventListener("input", () => {
     output.textContent = invite.value;
-});
+}); */
+
+let greeting = document.querySelector('.hello')
+
+let name = prompt("What is your name?");
+
+if ((name === 'Mason' || (name = 'mason'))) {
+    greeting.innerHTML = "very very smelly"
+}
+
+/* if ((name ==='gary' || (name = 'Gary'))) {
+    greeting.innerHTML = "FATHER, FATHER, FATHER, FATHER, FATHER"
+} */
